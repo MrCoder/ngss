3e16225cc5a437a7e111299e1d4e8f59ddf40ad3
Bernie Schelberg <bernie.schelberg@ausregistry.com.au>
2013-10-10 20:50:09 +1100

    implements ng-wait with scope.onready

--- assets/js/directives/ng-outlet.js
+++ assets/js/directives/ng-outlet.js
@@ -1,23 +1,26 @@
 // Since our ng-app is not named, we need to bind the directive to the 'ng' module
 var ngModule = angular.module('ng'); // get a module named 'ng', which is the default module.
 ngModule
-    .directive('ngOutlet', function ($window, $parse) {
+    .directive('ngOutlet', function ($window, $parse, $rootScope) {
         return {
             link: function (scope, element, attrs) {
-                var variableName = attrs['ngOutlet'];
+                var variableName = attrs.ngOutlet;
                 var model = $parse(variableName);
-                model.assign(scope, model($window));
+                model.assign($rootScope, model($window));
             }
         }
     })
     .directive('ngWait', function ($window, $compile, $timeout) {
         return {
             link: function (scope, element, attrs) {
-                element.compiled = false;
-                $timeout(function(){
-                    if (!element.compiled){
+                var persistence = this;
+                persistence.recompiled = !!persistence.recompiled;
+                scope.$whenReady(function(){
+                    console.log('this recompiled?', persistence.recompiled);
+                    if (!persistence.recompiled){
+                        console.log('re-compiling ...');
+                        persistence.recompiled = true;
                         $compile(element)(scope);
-                        element.compiled = true;
                     }
                 });
             }
